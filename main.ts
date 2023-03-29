bb7f9fe9df4a5daec2ef8dc2032889cc65866d8c
Jason Qin <68677082+Comprehensive-Jason@users.noreply.github.com>
2023-03-29 04:36:16 -0400

    Delete unnecessary comments

--- main.ts
+++ main.ts
@@ -13,10 +13,8 @@ import {
 import { CursorPos, clearScreenDown } from "readline";
 
 export default class MyPlugin extends Plugin {
-  //private detailLineRegex = /\[\^(\d+)\]\:/;
-  private NumReOnlyMarkers = /\[\^(\d+)\]/gi;
-  //private numericalRe = /(\d+)/;
 
+  private NumReOnlyMarkers = /\[\^(\d+)\]/gi;
   private NamedDetailLineRegex = /\[\^([^\[\]]+)\]:/;
   private NamedAllDetails = /\[\^([^\[\]]+)\]:/g;
   private NamedReOnlyMarkers = /\[\^([^\[\]]+)\](?!:)/dg;
@@ -41,15 +39,6 @@ export default class MyPlugin extends Plugin {
         this.insertNamedFootnote();
       }
     });
-    /*this.addCommand({
-      id: "Popup",
-      name: "Popup",
-      checkCallback: (checking: boolean) => {
-        if (checking)
-          return !!this.app.workspace.getActiveViewOfType(MarkdownView)
-        this.footnotePicker();
-      }
-    });*/
   }
 
   private listExistingFootnoteDetails(
@@ -413,113 +402,3 @@ export default class MyPlugin extends Plugin {
     
   }
 }
-
-
-//footnotePicker popup copied in from https://github.com/SilentVoid13/Templater
-/*
-export class footnotePicker extends EditorSuggest<TpSuggestDocumentation> {
-  private tp_keyword_regex =
-  /tp\.(?<module>[a-z]*)?(?<fn_trigger>\.(?<fn>[a-z_]*)?)?$/;
-  private documentation: Documentation;
-  private latest_trigger_info: EditorSuggestTriggerInfo;
-  private module_name: ModuleName | string;
-  private function_trigger: boolean;
-  private function_name: string;
-
-  constructor() {
-    super(app);
-    this.documentation = new Documentation();
-  }
-
-  onTrigger(
-      cursor: EditorPosition,
-      editor: Editor,
-      _file: TFile
-  ): EditorSuggestTriggerInfo | null {
-      const range = editor.getRange(
-          { line: cursor.line, ch: 0 },
-          { line: cursor.line, ch: cursor.ch }
-      );
-      const match = this.tp_keyword_regex.exec(range);
-      if (!match) {
-          return null;
-      }
-
-      let query: string;
-      const module_name = (match.groups && match.groups["module"]) || "";
-      this.module_name = module_name;
-
-      if (match.groups && match.groups["fn_trigger"]) {
-          if (module_name == "" || !is_module_name(module_name)) {
-              return null;
-          }
-          this.function_trigger = true;
-          this.function_name = match.groups["fn"] || "";
-          query = this.function_name;
-      } else {
-          this.function_trigger = false;
-          query = this.module_name;
-      }
-
-      const trigger_info: EditorSuggestTriggerInfo = {
-          start: { line: cursor.line, ch: cursor.ch - query.length },
-          end: { line: cursor.line, ch: cursor.ch },
-          query: query,
-      };
-      this.latest_trigger_info = trigger_info;
-      return trigger_info;
-  }
-
-  getSuggestions(context: EditorSuggestContext): TpSuggestDocumentation[] {
-      let suggestions: Array<TpSuggestDocumentation>;
-      if (this.module_name && this.function_trigger) {
-          suggestions = this.documentation.get_all_functions_documentation(
-              this.module_name as ModuleName
-          ) as TpFunctionDocumentation[];
-      } else {
-          suggestions = this.documentation.get_all_modules_documentation();
-      }
-      if (!suggestions) {
-          return [];
-      }
-      return suggestions.filter((s) => s.name.startsWith(context.query));
-  }
-
-  renderSuggestion(value: TpSuggestDocumentation, el: HTMLElement): void {
-      el.createEl("b", { text: value.name });
-      el.createEl("br");
-      if (this.function_trigger && is_function_documentation(value)) {
-          el.createEl("code", { text: value.definition });
-      }
-      if (value.description) {
-          el.createEl("div", { text: value.description });
-      }
-  }
-
-  selectSuggestion(
-      value: TpSuggestDocumentation,
-      _evt: MouseEvent | KeyboardEvent
-  ): void {
-      const active_view = app.workspace.getActiveViewOfType(MarkdownView);
-      if (!active_view) {
-          // TODO: Error msg
-          return;
-      }
-      active_view.editor.replaceRange(
-          value.name,
-          this.latest_trigger_info.start,
-          this.latest_trigger_info.end
-      );
-      if (
-          this.latest_trigger_info.start.ch == this.latest_trigger_info.end.ch
-      ) {
-          // Dirty hack to prevent the cursor being at the
-          // beginning of the word after completion,
-          // Not sure what's the cause of this bug.
-          const cursor_pos = this.latest_trigger_info.end;
-          cursor_pos.ch += value.name.length;
-          active_view.editor.setCursor(cursor_pos);
-      }
-  }
-}
-*/
