edae044450698d953e4908269da3b4b0ac434afe
Jason Qin <68677082+Comprehensive-Jason@users.noreply.github.com>
2023-03-29 04:32:10 -0400

    Add non-numerical footnote quick-entry Add hotkey for non-numerical footnotes Fix behavior for edge case:     - "What if... text has the same footnote at several places?"     - plugin will now behave as expected,       going to footnote detail when hotkey is pressed

--- main.ts
+++ main.ts
@@ -1,24 +1,111 @@
-import { Editor, EditorPosition, MarkdownView, Plugin } from "obsidian";
-import { clearScreenDown } from "readline";
+import { notEqual } from "assert";
+import { exec } from "child_process";
+import { 
+  Editor, 
+  EditorPosition, 
+  EditorSuggest, 
+  EditorSuggestContext,
+  EditorSuggestTriggerInfo,
+  MarkdownView, 
+  Plugin 
+} from "obsidian";
+
+import { CursorPos, clearScreenDown } from "readline";
 
 export default class MyPlugin extends Plugin {
-  private detailLineRegex = /\[\^(\d+)\]\:/;
-  private reOnlyMarkers = /\[\^(\d+)\]/gi;
-  private numericalRe = /(\d+)/;
+  //private detailLineRegex = /\[\^(\d+)\]\:/;
+  private NumReOnlyMarkers = /\[\^(\d+)\]/gi;
+  //private numericalRe = /(\d+)/;
+
+  private NamedDetailLineRegex = /\[\^([^\[\]]+)\]:/;
+  private NamedAllDetails = /\[\^([^\[\]]+)\]:/g;
+  private NamedReOnlyMarkers = /\[\^([^\[\]]+)\](?!:)/dg;
+  private NamedRe = /(?<=\[\^)([^\[\]]+)(?=\])/;
 
   async onload() {
     this.addCommand({
-      id: "insert-footnote",
-      name: "Insert and Navigate Footnote",
+      id: "insert-autonumbered-footnote",
+      name: "Insert / Navigate Auto-Numbered Footnote",
       checkCallback: (checking: boolean) => {
         if (checking)
           return !!this.app.workspace.getActiveViewOfType(MarkdownView);
-        this.insertFootnote();
+        this.insertAutonumFootnote();
       },
     });
+    this.addCommand({
+      id: "insert-named-footnote",
+      name: "Insert / Navigate Named Footnote",
+      checkCallback: (checking: boolean) => {
+        if (checking)
+          return !!this.app.workspace.getActiveViewOfType(MarkdownView);
+        this.insertNamedFootnote();
+      }
+    });
+    /*this.addCommand({
+      id: "Popup",
+      name: "Popup",
+      checkCallback: (checking: boolean) => {
+        if (checking)
+          return !!this.app.workspace.getActiveViewOfType(MarkdownView)
+        this.footnotePicker();
+      }
+    });*/
   }
 
-  insertFootnote() {
+  private listExistingFootnoteDetails(
+    doc: Editor
+  ) {
+    let FootnoteDetailList: string[] = [];
+    
+    //search each line for footnote details and add to list
+    for (let i = 0; i < doc.lineCount(); i++) {
+          let theLine = doc.getLine(i);
+          let lineMatch = theLine.match(this.NamedAllDetails);
+          if (lineMatch) {
+            let temp = lineMatch[0];
+            temp = temp.replace("[^","");
+            temp = temp.replace("]:","");
+
+            FootnoteDetailList.push(temp);
+          }
+        }
+    if (FootnoteDetailList.length > 0) {
+      return FootnoteDetailList;
+    } else {
+      return null;
+    }
+  }
+
+  private listExistingFootnoteMarkersAndLocations(
+    doc: Editor
+  ) {
+    type markerEntry = {
+      footnote: string;
+      lineNum: number;
+      startIndex: number;
+    }
+    let markerEntry;
+
+    let FootnoteMarkerInfo = [];
+    //search each line for footnote markers
+    //for each, add their name, line number, and start index to FootnoteMarkerInfo
+    for (let i = 0; i < doc.lineCount(); i++) {
+      let theLine = doc.getLine(i);
+      let lineMatch;
+
+      while ((lineMatch = this.NamedReOnlyMarkers.exec(theLine)) != null) {
+        markerEntry = {
+          footnote: lineMatch[0],
+          lineNum: i,
+          startIndex: lineMatch.index
+        }
+        FootnoteMarkerInfo.push(markerEntry);
+      }
+    }
+    return FootnoteMarkerInfo;
+  }
+  
+  insertAutonumFootnote() {
     const mdView = this.app.workspace.getActiveViewOfType(MarkdownView);
 
     if (!mdView) return false;
@@ -34,7 +121,7 @@ export default class MyPlugin extends Plugin {
     if (this.shouldJumpFromMarkerToDetail(lineText, cursorPosition, doc))
       return;
 
-    return this.shouldCreateNewFootnote(
+    return this.shouldCreateAutonumFootnote(
       lineText,
       cursorPosition,
       doc,
@@ -50,7 +137,7 @@ export default class MyPlugin extends Plugin {
     // check if we're in a footnote detail line ("[^1]: footnote")
     // if so, jump cursor back to the footnote in the text
     // https://github.com/akaalias/obsidian-footnotes#improved-quick-navigation
-    let match = lineText.match(this.detailLineRegex);
+    let match = lineText.match(this.NamedDetailLineRegex);
     if (match) {
       let s = match[0];
       let index = s.replace("[^", "");
@@ -82,24 +169,23 @@ export default class MyPlugin extends Plugin {
     doc: Editor
   ) {
     // Jump cursor TO detail marker
-    // check if the cursor is inside or left or right of a footnote in a line
-    // if so, jump cursor to the footnote detail line
-    // https://github.com/akaalias/obsidian-footnotes#improved-quick-navigation
 
     // does this line have a footnote marker?
     // does the cursor overlap with one of them?
     // if so, which one?
     // find this footnote marker's detail line
     // place cursor there
-    let reOnlyMarkersMatches = lineText.match(this.reOnlyMarkers);
-
     let markerTarget = null;
 
-    if (reOnlyMarkersMatches) {
-      for (let i = 0; i <= reOnlyMarkersMatches.length; i++) {
-        let marker = reOnlyMarkersMatches[i];
-        if (marker != undefined) {
-          let indexOfMarkerInLine = lineText.indexOf(marker);
+    let FootnoteMarkerInfo = this.listExistingFootnoteMarkersAndLocations(doc);
+    let currentLine = cursorPosition.line;
+    let footnotesOnLine = FootnoteMarkerInfo.filter(markerEntry => markerEntry.lineNum === currentLine);
+
+    if (footnotesOnLine != null && (footnotesOnLine.length-1 > 0)) {
+      for (let i = 0; i <= footnotesOnLine.length-1; i++) {
+        if (footnotesOnLine[i].footnote !== null) {
+          let marker = footnotesOnLine[i].footnote;
+          let indexOfMarkerInLine = footnotesOnLine[i].startIndex;
           if (
             cursorPosition.ch >= indexOfMarkerInLine &&
             cursorPosition.ch <= indexOfMarkerInLine + marker.length
@@ -110,23 +196,22 @@ export default class MyPlugin extends Plugin {
         }
       }
     }
-
-    if (markerTarget != null) {
+    if (markerTarget !== null) {
       // extract index
-      let match = markerTarget.match(this.numericalRe);
+      let match = markerTarget.match(this.NamedRe);
       if (match) {
         let indexString = match[0];
-        let markerIndex = Number(indexString);
+        //let markerIndex = Number(indexString);
 
         // find the first line with this detail marker index in it.
         for (let i = 0; i < doc.lineCount(); i++) {
           let theLine = doc.getLine(i);
-          let lineMatch = theLine.match(this.detailLineRegex);
+          let lineMatch = theLine.match(this.NamedDetailLineRegex);
           if (lineMatch) {
             // compare to the index
             let indexMatch = lineMatch[1];
-            let indexMatchNumber = Number(indexMatch);
-            if (indexMatchNumber == markerIndex) {
+            //let indexMatchNumber = Number(indexMatch);
+            if (indexMatch == indexString) {
               doc.setCursor({ line: i, ch: lineMatch[0].length + 1 });
               return true;
             }
@@ -137,14 +222,14 @@ export default class MyPlugin extends Plugin {
     return false;
   }
 
-  private shouldCreateNewFootnote(
+  private shouldCreateAutonumFootnote(
     lineText: string,
     cursorPosition: EditorPosition,
     doc: Editor,
     markdownText: string
   ) {
     // create new footnote with the next numerical index
-    let matches = markdownText.match(this.reOnlyMarkers);
+    let matches = markdownText.match(this.NumReOnlyMarkers);
     let numbers: Array<number> = [];
     let currentMax = 1;
 
@@ -191,7 +276,9 @@ export default class MyPlugin extends Plugin {
 
     let footnoteDetail = `\n[^${footNoteId}]: `;
 
-    if (currentMax == 1) {
+    let list = this.listExistingFootnoteDetails(doc);
+    
+    if (list===null && currentMax == 1) {
       footnoteDetail = "\n" + footnoteDetail;
       doc.setLine(doc.lastLine(), lastLine + footnoteDetail);
       doc.setCursor(doc.lastLine() - 1, footnoteDetail.length - 1);
@@ -200,4 +287,239 @@ export default class MyPlugin extends Plugin {
       doc.setCursor(doc.lastLine(), footnoteDetail.length - 1);
     }
   }
+
+
+  // Functions for Named Footnotes
+
+
+  insertNamedFootnote() {
+    const mdView = this.app.workspace.getActiveViewOfType(MarkdownView);
+
+    if (!mdView) return false;
+    if (mdView.editor == undefined) return false;
+
+    const doc = mdView.editor;
+    const cursorPosition = doc.getCursor();
+    const lineText = doc.getLine(cursorPosition.line);
+    const markdownText = mdView.data;
+
+    if (this.shouldJumpFromDetailToMarker(lineText, cursorPosition, doc))
+      return;
+    if (this.shouldJumpFromMarkerToDetail(lineText, cursorPosition, doc))
+      return;
+
+    if (this.shouldCreateMatchingFootnoteDetail(lineText, cursorPosition, doc))
+      return; 
+    return this.shouldCreateFootnoteMarker(
+      lineText,
+      cursorPosition,
+      doc,
+      markdownText
+    );
+
+
+  }
+  
+  private shouldCreateMatchingFootnoteDetail(
+    lineText: string,
+    cursorPosition: EditorPosition,
+    doc: Editor
+  ) {
+    // Create matching footnote detail for footnote marker
+    
+    // does this line have a footnote marker?
+    // does the cursor overlap with one of them?
+    // if so, which one?
+    // does this footnote marker have a detail line?
+    // if not, create it and place cursor there
+    let reOnlyMarkersMatches = lineText.match(this.NamedReOnlyMarkers);
+
+    let markerTarget = null;
+
+    if (reOnlyMarkersMatches){
+      for (let i = 0; i <= reOnlyMarkersMatches.length; i++) {
+        let marker = reOnlyMarkersMatches[i];
+        if (marker != undefined) {
+          let indexOfMarkerInLine = lineText.indexOf(marker);
+          if (
+            cursorPosition.ch >= indexOfMarkerInLine &&
+            cursorPosition.ch <= indexOfMarkerInLine + marker.length
+          ) {
+            markerTarget = marker;
+            break;
+          }
+        }
+      }
+    }
+
+    if (markerTarget != null) {
+      //extract footnote
+      let match = markerTarget.match(this.NamedRe)
+      //find if this footnote exists by listing existing footnote details
+      if (match) {
+        let footnoteId = match[0];
+        
+        let list: string[] = this.listExistingFootnoteDetails(doc);
+        
+        // Check if the list is empty OR if the list doesn't include current footnote
+        // if so, add detail for the current footnote
+        if(list === null || !list.includes(footnoteId)) {
+          let lastLineIndex = doc.lastLine();
+          let lastLine = doc.getLine(lastLineIndex);
+
+          while (lastLineIndex > 0) {
+            lastLine = doc.getLine(lastLineIndex);
+            if (lastLine.length > 0) {
+              doc.replaceRange(
+                "",
+                { line: lastLineIndex, ch: 0 },
+                { line: doc.lastLine(), ch: 0 }
+              );
+              break;
+            }
+            lastLineIndex--;
+          }
+          
+          let footnoteDetail = `\n[^${footnoteId}]: `;
+                    
+          if (list===null || list.length < 1) {
+            footnoteDetail = "\n" + footnoteDetail;
+            doc.setLine(doc.lastLine(), lastLine + footnoteDetail);
+            doc.setCursor(doc.lastLine() - 1, footnoteDetail.length - 1);
+          } else {
+            doc.setLine(doc.lastLine(), lastLine + footnoteDetail);
+            doc.setCursor(doc.lastLine(), footnoteDetail.length - 1);
+          }
+
+          return true;
+        }
+        return; 
+      }
+    }
+  }
+
+  private shouldCreateFootnoteMarker(
+    lineText: string,
+    cursorPosition: EditorPosition,
+    doc: Editor,
+    markdownText: string
+  ) {
+    //create empty footnote marker for name input
+    let emptyMarker = `[^]`;
+    doc.replaceRange(emptyMarker,doc.getCursor());
+    //move cursor in between [^ and ]
+    doc.setCursor(cursorPosition.line, cursorPosition.ch+2);
+    //open footnotePicker popup
+    
+  }
 }
+
+
+//footnotePicker popup copied in from https://github.com/SilentVoid13/Templater
+/*
+export class footnotePicker extends EditorSuggest<TpSuggestDocumentation> {
+  private tp_keyword_regex =
+  /tp\.(?<module>[a-z]*)?(?<fn_trigger>\.(?<fn>[a-z_]*)?)?$/;
+  private documentation: Documentation;
+  private latest_trigger_info: EditorSuggestTriggerInfo;
+  private module_name: ModuleName | string;
+  private function_trigger: boolean;
+  private function_name: string;
+
+  constructor() {
+    super(app);
+    this.documentation = new Documentation();
+  }
+
+  onTrigger(
+      cursor: EditorPosition,
+      editor: Editor,
+      _file: TFile
+  ): EditorSuggestTriggerInfo | null {
+      const range = editor.getRange(
+          { line: cursor.line, ch: 0 },
+          { line: cursor.line, ch: cursor.ch }
+      );
+      const match = this.tp_keyword_regex.exec(range);
+      if (!match) {
+          return null;
+      }
+
+      let query: string;
+      const module_name = (match.groups && match.groups["module"]) || "";
+      this.module_name = module_name;
+
+      if (match.groups && match.groups["fn_trigger"]) {
+          if (module_name == "" || !is_module_name(module_name)) {
+              return null;
+          }
+          this.function_trigger = true;
+          this.function_name = match.groups["fn"] || "";
+          query = this.function_name;
+      } else {
+          this.function_trigger = false;
+          query = this.module_name;
+      }
+
+      const trigger_info: EditorSuggestTriggerInfo = {
+          start: { line: cursor.line, ch: cursor.ch - query.length },
+          end: { line: cursor.line, ch: cursor.ch },
+          query: query,
+      };
+      this.latest_trigger_info = trigger_info;
+      return trigger_info;
+  }
+
+  getSuggestions(context: EditorSuggestContext): TpSuggestDocumentation[] {
+      let suggestions: Array<TpSuggestDocumentation>;
+      if (this.module_name && this.function_trigger) {
+          suggestions = this.documentation.get_all_functions_documentation(
+              this.module_name as ModuleName
+          ) as TpFunctionDocumentation[];
+      } else {
+          suggestions = this.documentation.get_all_modules_documentation();
+      }
+      if (!suggestions) {
+          return [];
+      }
+      return suggestions.filter((s) => s.name.startsWith(context.query));
+  }
+
+  renderSuggestion(value: TpSuggestDocumentation, el: HTMLElement): void {
+      el.createEl("b", { text: value.name });
+      el.createEl("br");
+      if (this.function_trigger && is_function_documentation(value)) {
+          el.createEl("code", { text: value.definition });
+      }
+      if (value.description) {
+          el.createEl("div", { text: value.description });
+      }
+  }
+
+  selectSuggestion(
+      value: TpSuggestDocumentation,
+      _evt: MouseEvent | KeyboardEvent
+  ): void {
+      const active_view = app.workspace.getActiveViewOfType(MarkdownView);
+      if (!active_view) {
+          // TODO: Error msg
+          return;
+      }
+      active_view.editor.replaceRange(
+          value.name,
+          this.latest_trigger_info.start,
+          this.latest_trigger_info.end
+      );
+      if (
+          this.latest_trigger_info.start.ch == this.latest_trigger_info.end.ch
+      ) {
+          // Dirty hack to prevent the cursor being at the
+          // beginning of the word after completion,
+          // Not sure what's the cause of this bug.
+          const cursor_pos = this.latest_trigger_info.end;
+          cursor_pos.ch += value.name.length;
+          active_view.editor.setCursor(cursor_pos);
+      }
+  }
+}
+*/
